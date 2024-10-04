f7c5e9b76c8f28df970917be51ef5d7af4387cd8
Discussions <80537729+discus-sions@users.noreply.github.com>
2024-10-03 20:44:37 -0500

    Update GameModeTranslations.cs

--- src/GameModes/GameModeTranslations.cs
+++ src/GameModes/GameModeTranslations.cs
@@ -1,7 +1,9 @@
 using VentLib.Localization.Attributes;
+
 namespace Lotus.GameModes;
 
-public static class GamemodeTranslations
+[Localized("Options.GameMode")]
+public class GamemodeTranslations
 {
     [Localized(nameof(GamemodeText))] public static string GamemodeText = "GameMode";
 
