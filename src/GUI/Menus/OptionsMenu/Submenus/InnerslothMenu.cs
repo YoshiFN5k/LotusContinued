1fe6f28373ad66d7b50b3890bdbfe5b65e61e215
Discussions <80537729+discus-sions@users.noreply.github.com>
2025-02-06 19:02:24 -0600

    Restore Among Us Data Menu

--- src/GUI/Menus/OptionsMenu/Submenus/InnerslothMenu.cs
+++ src/GUI/Menus/OptionsMenu/Submenus/InnerslothMenu.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 using VentLib.Utilities;
 using VentLib.Utilities.Attributes;
 using Lotus.GUI.Menus.OptionsMenu.Components;
-using Lotus.Options;
+using Lotus.GUI.Menus.OptionsMenu.Patches;
+using Lotus.Logging;
+using VentLib.Utilities.Extensions;
 
 namespace Lotus.GUI.Menus.OptionsMenu.Submenus;
 
@@ -14,6 +16,10 @@ public class InnerslothMenu : MonoBehaviour, IBaseOptionMenuComponent
 {
     private TextMeshPro titleHeader;
     private GameObject anchorObject;
+
+    private MonoToggleButton privacyPolicyLink;
+    private MonoToggleButton termsOfUseLink;
+    private MonoToggleButton dataCollection;
 
     public InnerslothMenu(IntPtr intPtr) : base(intPtr)
     {
@@ -35,6 +41,72 @@ public class InnerslothMenu : MonoBehaviour, IBaseOptionMenuComponent
     public void PassMenu(OptionsMenuBehaviour menuBehaviour)
     {
         anchorObject.SetActive(false);
+
+        PassiveButton dataCollectionButton = menuBehaviour.FindChild<PassiveButton>("DataCollectionButton", true);
+
+        bool openDataCollection = false; // pl auto runs the action when it is made visible. so we keep track of the first visit to stop this.
+
+        GameObject dataCollectionObject = new("Manage Data Collection");
+        dataCollectionObject.transform.SetParent(anchorObject.transform);
+        dataCollectionObject.transform.localScale = new Vector3(1f, 1f, 1f);
+        dataCollection = dataCollectionObject.AddComponent<MonoToggleButton>();
+        dataCollection.ConfigureAsPressButton("Manage Data Collection", () =>
+        {
+            if (!openDataCollection)
+            {
+                openDataCollection = true;
+                return;
+            }
+            dataCollectionButton.OnClick.Invoke();
+        });
+        dataCollectionObject.transform.localPosition = new Vector3(0.029f, 0.353f, -1f);
+
+        bool openPrivacyLink = false; // pl auto runs the action when it is made visible. so we keep track of the first visit to stop this.
+
+        GameObject privacyPolicyLinkObject = new("Privacy Policy Link");
+        privacyPolicyLinkObject.transform.SetParent(anchorObject.transform);
+        privacyPolicyLinkObject.transform.localScale = new Vector3(1f, 1f, 1f);
+        privacyPolicyLink = privacyPolicyLinkObject.AddComponent<MonoToggleButton>();
+        privacyPolicyLink.ConfigureAsPressButton("View Privacy Policy", () =>
+        {
+            if (!openPrivacyLink)
+            {
+                openPrivacyLink = true;
+                return;
+            }
+            menuBehaviour.OpenPrivacyPolicy();
+        });
+        privacyPolicyLinkObject.transform.localPosition = new Vector3(0.029f, -1.014f, -1f);
+
+        bool openTermsOfUseLink = false; // pl auto runs the action when it is made visible. so we keep track of the first visit to stop this.
+
+        GameObject termsOfUseLinkObject = new("Terms Of Use Link");
+        termsOfUseLinkObject.transform.SetParent(anchorObject.transform);
+        termsOfUseLinkObject.transform.localScale = new Vector3(1f, 1f, 1f);
+        termsOfUseLink = termsOfUseLinkObject.AddComponent<MonoToggleButton>();
+        termsOfUseLink.ConfigureAsPressButton("View Terms Of Use", () =>
+        {
+            if (!openTermsOfUseLink)
+            {
+                openTermsOfUseLink = true;
+                return;
+            }
+            menuBehaviour.OpenTermsOfUse();
+        });
+        termsOfUseLinkObject.transform.localPosition = new Vector3(0.029f, -2.381f, -1f);
+
+        // bring over Innersloth stuff
+        PassiveButton twitchButton = menuBehaviour.FindChild<PassiveButton>("TwitchLinkButton", true);
+        PassiveButton puidButton = menuBehaviour.FindChild<PassiveButton>("ShowPUID", true);
+
+        twitchButton.transform.SetParent(anchorObject.transform);
+        puidButton.transform.SetParent(anchorObject.transform);
+
+        twitchButton.transform.localScale = Vector3.one * 0.3f;
+        twitchButton.transform.localPosition = new Vector3(1.848f, -1.695f, -1f);
+
+        puidButton.transform.localScale = Vector3.one;
+        puidButton.transform.localPosition = new Vector3(-1.1f, -4.548f, -1);
     }
 
     public virtual void Open()
